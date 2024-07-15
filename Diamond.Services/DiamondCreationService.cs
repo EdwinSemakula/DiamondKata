ec94ae186609c219928e46ee87781cfd3b1ef30f
Edwin Semakula <edwinsemakula@tfl.gov.uk>
2024-07-15 13:37:19 +0100

    feat: added services for validation and diamond creation

---
+++ Diamond.Services/DiamondCreationService.cs
@@ -0,0 +1,59 @@
+﻿using Diamond.Common;
+using Diamond.Services.Interfaces;
+using System.Text;
+
+namespace Diamond.Services;
+
+public class DiamondCreationService : IDiamondCreationService
+{
+    private char[] _alphabet;
+
+    public string[] CreateDiamond(char input)
+    {
+        _alphabet = AlphabetArray.Alphabet;
+
+        var inputIndex = Array.IndexOf(_alphabet, input);
+        var diamondArraySize = (2 * inputIndex) + 1;
+        var diamond = new string[diamondArraySize];
+
+        for (var i = 0; i <= inputIndex; i++)
+        {
+            diamond[i] = CreateDiamondRow(i, inputIndex);
+
+            if (i < inputIndex)
+            {
+                diamond[diamondArraySize - i - 1] = diamond[i];
+            }
+        }
+
+        return diamond;
+    }
+
+    private string CreateDiamondRow(int rowIndex, int inputIndex)
+    {
+        var row = new StringBuilder();
+
+        // add initial spaces
+        for (int j = 0; j < inputIndex - rowIndex; j++)
+        {
+            row.Append(" ");
+        }
+
+        // add letter
+        row.Append(_alphabet[rowIndex]);
+
+        // add spaces between letters
+        if (rowIndex > 0)
+        {
+            for (int j = 0; j < (rowIndex * 2) - 1; j++)
+            {
+                row.Append(" ");
+            }
+
+            // add letter
+            row.Append(_alphabet[rowIndex]);
+        }
+
+        return row.ToString();
+    }
+}
---
+++ Diamond.Services/Validators/DiamondRequestValidation.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+using Diamond.Services.Interfaces;
+
+namespace Diamond.Services.Validators;
+
+public class DiamondRequestValidation : IDiamondRequestValidation
+{
+    public bool ValidateInput(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Please enter a character from A to Z");
+            return false;
+        }
+
+        if (input.Length > 1)
+        {
+            Console.WriteLine("Please enter only one character");
+            return false;
+        }
+
+        if (IsLetter(input))
+            return true;
+
+        Console.WriteLine("Input must be a character from A to Z");
+        return false;
+
+    }
+
+    private bool IsLetter(string input)
+    {
+        return Regex.IsMatch(input, @"^[a-zA-Z]+$");
+    }
+}
