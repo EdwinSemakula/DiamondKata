a224f1d18092f6b58e9114242f161f46eb6bc007
Edwin Semakula <edwinsemakula@tfl.gov.uk>
2024-07-15 13:37:53 +0100

    feat: implemented service to console app

--- Diamond.Console/Program.cs
+++ Diamond.Console/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Diamond.Console
+﻿using Diamond.Services;
+using Diamond.Services.Interfaces;
+using Diamond.Services.Validators;
+
+namespace Diamond.Console
 {
     public class Program
     {
+        private static IDiamondCreationService _diamondService;
+        private static IDiamondRequestValidation _diamondRequestValidation;
+        public Program(IDiamondCreationService diamondCreationService,
+            IDiamondRequestValidation diamondRequestValidation)
+        {
+            _diamondService = diamondCreationService;
+            _diamondRequestValidation = diamondRequestValidation;
+        }
+
         public static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello world");
+
+            var diamondCreationService = new DiamondCreationService();
+            var diamondRequestValidation = new DiamondRequestValidation();
+            var program = new Program(diamondCreationService, diamondRequestValidation);
+
+            string stringInput;
+
+            do
+            {
+                System.Console.WriteLine("Please enter a letter of the alphabet:");
+                stringInput = System.Console.ReadLine() ?? "";
+            } while (!_diamondRequestValidation.ValidateInput(stringInput));
+
+            if (_diamondRequestValidation.ValidateInput(stringInput))
+            {
+                var input = stringInput.ToUpper()[0];
+                var diamond = _diamondService.CreateDiamond(input);
+
+                foreach (var letter in diamond)
+                {
+                    System.Console.WriteLine(letter);
+                }
+            }
         }
     }
 }
